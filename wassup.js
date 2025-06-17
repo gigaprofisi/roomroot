21118ef70892305d65134326516d4344f2b61746
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2025-06-16 20:45:59 -0700

    Update wassup.js

--- wassup.js
+++ wassup.js
@@ -1,11 +1,5 @@
-
-    $(function() {
-
-
-
-   $.getJSON('./splashes.json', function(d) {
-       document.getElementById("splash").innerHTML=d[Math.floor(Math.random()*d.length)]
-
-   })
-
+$(function() {
+  $.getJSON('./splashes.json', function(d){
+    document.getElementById("splash").innerHTML=d[Math.floor(Math.random()*d.length)]
+  })
 })
