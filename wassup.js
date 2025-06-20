a109ca44e76c6d6ac2dc39745465f21725cae608
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2025-06-19 22:01:33 -0700

    Update wassup.js

--- wassup.js
+++ wassup.js
@@ -1,5 +1,5 @@
 $(function() {
-    document.getElementById("splash").innerHTML=splashes[Math.floor(Math.random()*d.length)]
+    document.getElementById("splash").innerHTML=splashes[Math.floor(Math.random()*splashes.length)]
 })
 
 throw "ball, go catch it Good boy"
