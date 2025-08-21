ae46bbb6fab8254a772d7e8a6655eb3e4e7a8ad3
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2025-08-20 22:43:06 -0700

    Create frankel1.js

---
+++ data/frankel1.js
@@ -0,0 +1,26 @@
+const frankel1 = `"Well, if I really were to assign blame
+I would assign the blame to myself, and my colleagues, professional mathematicians.
+We don't do nearly enough exposing these ideas to the public in an accessible way.
+Often times we are not willing to come up with good metaphors and analogies
+And this is how science should be communicated
+You have to communicate science in a way that's creative, that links to something people already know
+We are not doing nearly enough
+
+/MySolutionIsThatMoreMathematiciansShouldGoOutAndLetOtherPeopleInOnTheSecret
+/ItsReallyAlmostLikeASecret+AndWeAreAlmostLikeThisEliteWhichIsKeepingThatSecret
+And I don't want that to continue.
+I want everyone in on the secret. I want everyone to realize there is this beautiful world out there, which is so important for our daily lives
+Which posesses this unparalleled power and beauty.
+We have to convey this idea. We have to find ways to convey those ideas.
+Why has it not already happened? Well sometimes I wonder myself, why has it not already happened.
+It's almost a conspiracy... It's almost like there's a system of mirrors which distorts reality
+which does not allow most people to see what is out there
+And lets face it, how many more things are left to discover?
+There is this beautiful world out there, and you don't even have to travel anywhere to find it.
+It's right at your fingertips.
+Wouldn't you want to go and find out more about it?
+
+This is the coolest stuff in the world.
+And yet everyone hates it. Isn't it ironic?"
+
+- Edward Frankel, Numberphile, "Why do people hate mathematics?"`
