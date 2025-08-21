76841cfd5d67456f544464799602d1a9712ce105
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2025-08-21 00:35:42 -0700

    Update frankel1.js

--- data/frankel1.js
+++ data/frankel1.js
@@ -1,27 +1,34 @@
 
-export function frankel_1() {return `"Well, if I really were to assign blame
-I would assign the blame to myself, and my colleagues, professional mathematicians.
-We don't do nearly enough exposing these ideas to the public in an accessible way.
-Often times we are not willing to come up with good metaphors and analogies
-And this is how science should be communicated
-You have to communicate science in a way that's creative, that links to something people already know
-We are not doing nearly enough
+export function frankel_1() {return `"Well, if I really were to assign blame, I would assign the blame to myself and my colleagues; professional mathematicians.
+We don't do nearly enough, exposing these ideas to the public in an accessible way.
+Often times we're not willing to come up with good metaphors and analogies, and this is how science should be communicated.
+You have to communicate science in a way that's creative, that links to something people already know.
+And I feel as though other scientists are doing a much better job, physicists, biologists.
+We keep talking about the solar system, about the universe, about galaxies, about atoms, and molecules, and elementary particles, and DNA.
+Those concepts are no more complicated than things that we do in modern mathematics.
+Why is it that DNA and stars and elementary particles are part of our cultural discourse, but mathematical ideas are not?
+In part because we are not doing nearly enough. We professional mathematicians are not doing nearly enough.
+I don't blame the teachers, because they are in the precarious, really unenviable position because they are overworked and underpaid, and teachers themselves are the products of the same flawed system.
 
-/MySolutionIsThatMoreMathematiciansShouldGoOutAndLetOtherPeopleInOnTheSecret
-/ItsReallyAlmostLikeASecret+AndWeAreAlmostLikeThisEliteWhichIsKeepingThatSecret
-And I don't want that to continue.
-I want everyone in on the secret. I want everyone to realize there is this beautiful world out there, which is so important for our daily lives
-Which posesses this unparalleled power and beauty.
-We have to convey this idea. We have to find ways to convey those ideas.
+My solution is that, first of all, more mathematicians should go out and let other people in on the secret.
+Because it's really almost like a secret, and we're almost like this elite which is keeping that secret, and I don't want that to continue.
+I want everyone in on the secret. I want everyone to realize that there is this beautiful world out there, which is so important for our daily lives, which posesses this unparalleled power and beauty.
+We have to convey these ideas; we have to find ways to convey those ideas.
+And then we also have to work through our education system.
+We have to help teachers learn the nescessary content and to learn how mathematics connects with the real world, so that they make the study of mathematics for students more enjoyable, more fun, more interesting.
+
 Why has it not already happened? Well sometimes I wonder myself, why has it not already happened.
-It's almost a conspiracy... It's almost like there's a system of mirrors which distorts reality
-which does not allow most people to see what is out there
-And lets face it, how many more things are left to discover?
-There is this beautiful world out there, and you don't even have to travel anywhere to find it.
+It's almost a conspiracy, I mean honestly... it's almost like there's a system of mirrors that has been created which distorts reality.
+Which does not allow most people to see what's out there.
+And let's face it, how many more things are left to discover?
+We can't be the first to discover another continent, we can't be the first to set foot on the moon.
+But what if I told you that there is this beautiful world out there, and you don't even have to travel anywhere to find it.
 It's right at your fingertips.
 Wouldn't you want to go and find out more about it?
 
-This is the coolest stuff in the world.
+And if you do this in the right way, you will have children, you will have students running to class!
+Not away from the class, but running to class, ''I want to know more. I want to find out more. I want to talk to my peers, to my classmates about this stuff.''
+This is the coolest stuff in the world, that's what it is.
 And yet everyone hates it. Isn't it ironic?"
 
-- Edward Frankel, Numberphile, "Why do people hate mathematics?"`}
+Edward Frankel, Numberphile, "Why do people hate mathematics?"`}
