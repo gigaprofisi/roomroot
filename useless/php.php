f21bf71110415505a6a6c7452117ddd722bc6233
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2025-08-22 21:59:15 -0700

    Update and rename asm.asm to php.php

--- useless/php.php
+++ useless/php.php
@@ -6,7 +6,7 @@ EVERYTHIng is a lie believe that. infarct i dont think you even known, Im a frau
 EVERYTHIng is a lie believe that. infarct i dont think you even known, Im a fraud actually. Im not supposed to be here. This is the bad place! Holy fork or something.. Idk
 EVERYTHIng is a lie believe that. infarct i dont think you even known, Im a fraud actually. Im not supposed to be here. This is the bad place! Holy fork or something.. Idk
 
-
+ASSEMBLY! yall i swear , asemly is Awesome. I would fuck it if i could if you know what i mean lol. I mean assembly didnt work so this is the php stupidass file now. But you get it i think
 
 
 
