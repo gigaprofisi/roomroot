3cf152c03cdde36281d144e1b70a57fc1921eb71
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2025-08-22 21:57:51 -0700

    Create asm.asm

---
+++ useless/asm.asm
@@ -0,0 +1,20 @@
+baaaaaaah
+baaaaaaah
+garbage data
+garbage data
+EVERYTHIng is a lie believe that. infarct i dont think you even known, Im a fraud actually. Im not supposed to be here. This is the bad place! Holy fork or something.. Idk
+EVERYTHIng is a lie believe that. infarct i dont think you even known, Im a fraud actually. Im not supposed to be here. This is the bad place! Holy fork or something.. Idk
+EVERYTHIng is a lie believe that. infarct i dont think you even known, Im a fraud actually. Im not supposed to be here. This is the bad place! Holy fork or something.. Idk
+
+
+
+
+
+
+
+
+
+
+
+
+LOL, jokin.
